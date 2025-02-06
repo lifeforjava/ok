a759601f7bd6b61e2b01ad23d3c2bcd472e2b252
ankul <lifeforjava786@gmail.com>
2025-02-06 09:59:04 +0530

    still working on project ok

--- src/main/java/com/app/hcl/stock_exchange_service/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.hcl.stock_exchange_service.repository;
-
-import com.app.hcl.stock_exchange_service.entity.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
